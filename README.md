4cd68232128d912a77cd71014be8697355f116cc
enlightflux <enlightflux@gmail.com>
2024-11-06 23:39:34 +0545

    Added UI to the Game

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # My First JavaScript Practice file
+
 Project required on The Odin Project.
+
+# Voila! It has been Updated!
+
+Updated with brand new UI.
--- script.js
+++ script.js
@@ -1,65 +1,157 @@
-// Computer's Selection
+// overall Scores
+let computerOverallScore = 0;
+let userOverallScore = 0;
+
+//  Selection values
 const rock = 1;
 const paper = 2;
 const scissor = 3;
+//
+// current selection
+let userChoice = null;
+let computerChoice = null;
+// user's selection
+const rockSelection = document.querySelector('.rockSelection');
+const paperSelection = document.querySelector('.paperSelection');
+const scissorSelection = document.querySelector('.scissorSelection');
+//
+
+const getUserCurrentSelection = document.querySelector('.yourselection');
+//
+rockSelection.addEventListener('click', rockSelected);
+
+function rockSelected() {
+  getUserCurrentSelection.textContent = '';
+  const rockClicked = document.createElement('p');
+  rockClicked.textContent = 'rock';
+  rockClicked.classList.add('rock');
+  getUserCurrentSelection.appendChild(rockClicked);
+  userChoice = rock;
+  computerSelection();
+  playEachRound();
+}
+
+paperSelection.addEventListener('click', paperSelected);
+
+function paperSelected() {
+  getUserCurrentSelection.textContent = '';
+  const paperClicked = document.createElement('p');
+  paperClicked.textContent = 'paper';
+  paperClicked.classList.add('paper');
+  getUserCurrentSelection.appendChild(paperClicked);
+  userChoice = paper;
+  computerSelection();
+  playEachRound();
+}
+
+scissorSelection.addEventListener('click', scissorSelected);
+
+function scissorSelected() {
+  getUserCurrentSelection.textContent = '';
+  const scissorClicked = document.createElement('p');
+  scissorClicked.textContent = 'scissor';
+  scissorClicked.classList.add('scissor');
+  getUserCurrentSelection.appendChild(scissorClicked);
+  userChoice = scissor;
+  computerSelection();
+  playEachRound();
+}
 
 //
 //
-let humanScore = 0;
-let computerScore = 0;
-//
-//
+const getComputerCurrentSelection =
+  document.querySelector('.computerSelection');
 
 function getComputerChoice() {
   return Math.floor(Math.random() * 3) + 1;
 }
 
-function getHumanChoice() {
-  return prompt("Please type Rock, Paper, or Scissor:");
-}
-
-function playRound() {
-  let computerChoice = getComputerChoice();
-  let userInput = getHumanChoice();
-  let humanChoice = userInput.toLowerCase().trim();
-  if (
-    humanChoice != "rock" &&
-    humanChoice != "paper" &&
-    humanChoice != "scissor"
-  ) {
-    return "Invalid input. Please type Rock, paper or scissor";
-  }
-  if (computerChoice === paper && humanChoice === "rock") {
-    computerScore = computerScore + 1;
-    return "Computer Wins. Paper beats rock.";
-  } else if (computerChoice === paper && humanChoice === "scissor") {
-    humanScore = humanScore + 1;
-    return "You win. Scissor beats paper.";
-  } else if (computerChoice === rock && humanChoice === "paper") {
-    humanScore = humanScore + 1;
-    return "You win. Paper beats rock.";
-  } else if (computerChoice === rock && humanChoice === "scissor") {
-    computerScore = computerScore + 1;
-    return "Computer Wins. Rock beats scissor.";
-  } else if (computerChoice === scissor && humanChoice === "rock") {
-    humanScore = humanScore + 1;
-    return "You win. Rock beats scissor.";
-  } else if (computerChoice === scissor && humanChoice === "paper") {
-    computerScore = computerScore + 1;
-    return "Computer Wins. Scissor beats paper.";
+function computerSelection() {
+  const choosenValue = getComputerChoice();
+  if (choosenValue === 1) {
+    getComputerCurrentSelection.textContent = '';
+    const rockChoosen = document.createElement('p');
+    rockChoosen.textContent = 'rock';
+    rockChoosen.classList.add('rock');
+    getComputerCurrentSelection.appendChild(rockChoosen);
+    computerChoice = rock;
+  } else if (choosenValue === 2) {
+    getComputerCurrentSelection.textContent = '';
+    const paperChoosen = document.createElement('p');
+    paperChoosen.textContent = 'paper';
+    paperChoosen.classList.add('paper');
+    getComputerCurrentSelection.appendChild(paperChoosen);
+    computerChoice = paper;
   } else {
-    return `It's a draw. you both selected ${humanChoice}.`;
+    getComputerCurrentSelection.textContent = '';
+    const scissorChoosen = document.createElement('p');
+    scissorChoosen.textContent = 'scissor';
+    scissorChoosen.classList.add('scissor');
+    getComputerCurrentSelection.appendChild(scissorChoosen);
+    computerChoice = scissor;
   }
 }
+//
+//
+//
+const currentResultMessage = document.querySelector('.currentResultMessage');
 
-//
-//
-//
-function start() {
-  console.log(playRound());
-  console.log(`Computer: ${computerScore} | Human: ${humanScore}`);
-}
+function playEachRound() {
+  if (computerChoice === userChoice) {
+    currentResultMessage.textContent = '';
+    const message = document.createElement('h1');
+    message.textContent = "Hmm... It's a Draw.!";
+    message.classList.add('draw');
 
-for (let i = 0; i < 5; i++) {
-  start();
+    currentResultMessage.appendChild(message);
+  } else if (computerChoice === rock && userChoice === paper) {
+    currentResultMessage.textContent = '';
+    const message = document.createElement('h1');
+    message.textContent = 'Yay! You Win!';
+    message.classList.add('win');
+    userOverallScore++;
+    currentResultMessage.appendChild(message);
+  } else if (computerChoice === rock && userChoice === scissor) {
+    currentResultMessage.textContent = '';
+    const message = document.createElement('h1');
+    message.textContent = 'Oh No! You Lost!';
+    message.classList.add('lost');
+    computerOverallScore++;
+    currentResultMessage.appendChild(message);
+  } else if (computerChoice === paper && userChoice === rock) {
+    currentResultMessage.textContent = '';
+    const message = document.createElement('h1');
+    message.textContent = 'Oh No! You Lost!';
+    message.classList.add('lost');
+    computerOverallScore++;
+    currentResultMessage.appendChild(message);
+  } else if (computerChoice === paper && userChoice === scissor) {
+    currentResultMessage.textContent = '';
+    const message = document.createElement('h1');
+    message.textContent = 'Yay! You Win!';
+    message.classList.add('win');
+    userOverallScore++;
+    currentResultMessage.appendChild(message);
+  } else if (computerChoice === scissor && userChoice === rock) {
+    currentResultMessage.textContent = '';
+    const message = document.createElement('h1');
+    message.textContent = 'Yay! You Win!';
+    message.classList.add('win');
+    userOverallScore++;
+    currentResultMessage.appendChild(message);
+  } else if (computerChoice === scissor && userChoice === paper) {
+    currentResultMessage.textContent = '';
+    const message = document.createElement('h1');
+    message.textContent = 'Oh No! You Lost!';
+    message.classList.add('lost');
+    computerOverallScore++;
+    currentResultMessage.appendChild(message);
+  }
+  // Overall Score Stat
+  document.querySelector(
+    '.computer'
+  ).textContent = `Computer: ${computerOverallScore}`;
+  document.querySelector('.human').textContent = `You: ${userOverallScore}`;
+  document.querySelector('.winrate').textContent = `Your current win rate is %`;
 }
+//
